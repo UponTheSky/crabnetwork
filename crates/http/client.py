23595d09c4b8351312902d5c49dfe2f460ea7cbb
UponTheSky <sailing.to.orbis@gmail.com>
2025-03-12 00:52:56 +0900

    http: improve client script

--- crates/http/client.py
+++ crates/http/client.py
@@ -1,20 +1,33 @@
 import socket
+import sys
 
 
 def make_tcp_request(host: str, port: int) -> None:
+    sock: socket.socket
     ip_version: int # ipv4 vs v6
-    conn_type: socket.SocketKind # tcp vs udp
+    tcp_or_udp: socket.SocketKind # tcp vs udp
 
     # getaddrinfo: tcp & ipv4+v6
-    for family, type, _, _, addr in socket.getaddrinfo(host, port, socket.AF_UNSPEC, socket.SOCK_STREAM):
-        # create a socket
-        if addr is not None:
-            ip_version, conn_type = family, type
-            break
+    for ip_version, tcp_or_udp, _, _, _  in socket.getaddrinfo(host, port, socket.AF_UNSPEC, socket.SOCK_STREAM):
+        try:
+            # try to create a socket and connect for the given addr info
+            sock = socket.socket(ip_version, tcp_or_udp)
+            sock.connect((host, port))
 
-    # connect
-    with socket.socket(ip_version, conn_type) as sock:
-        sock.connect((host, port))
+        except OSError as error:
+            print(error)
+            if sock:
+                sock.close()
+                continue
+
+        break
+
+    if not sock:
+        print(f"no connection available for (host, port): {(host, port)}")
+        sys.exit(1)
+
+    # send & receive data
+    with sock:
         sock.sendall(b"Hi from client!")
         
         if received := sock.recv(1024):
